8ed8b332af4e669d0eab1b1e3453d29594d50f80
Will Russell <will@wrussell.co.uk>
2020-06-28 23:19:08 +0100

    Update styles

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ load_dotenv(os.path.join(FOLDER, '.env'))
 SECRET_KEY = os.getenv("KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com', '.wrussell.co.uk']
 
